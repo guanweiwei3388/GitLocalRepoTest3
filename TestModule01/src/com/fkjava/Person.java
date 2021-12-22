060ba7bcb2945a2ebfd1f50a7a478b46ec411045
关伟伟 <ghwzsy@163.com>
2021-12-22 20:55:25 +0800

    新增Person类

---
+++ TestModule01/src/com/fkjava/Person.java
@@ -0,0 +1,13 @@
+package com.fkjava;
+
+/**
+ * @Version 1.0
+ * @Description:
+ * @Author:G
+ * @Date:2021/12/22
+ * @Time:20:49
+ */
+public class Person {
+    private int age;
+    private String name;
+}
